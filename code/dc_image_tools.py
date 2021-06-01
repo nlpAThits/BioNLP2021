3178dc3aa7db4fd209c738666e0120caa73591e4
hits-nlp <mark-christoph.mueller@h-its.org>
2021-06-01 12:23:28 +0200

    os.remove --> os.replace

--- code/dc_image_tools.py
+++ code/dc_image_tools.py
@@ -339,22 +339,22 @@ def pdf_to_pngs(pdf_file, out_dir="./", save_as_base="", dpi=300, force_new=Fals
         newly_created=True
         # This will overwrite any existing files of the same name
         subprocess.check_output(["pdftocairo", "-r", str(dpi), "-png", pdf_file, out_dir+"/"+out_file_base])
-    if newly_created:
-        print("\t\tRenaming images ...", file=sys.stderr)
-        # Get all images relating to the current file. 
-        # This will also include any *previous* results that were renamed already!
-        for i, png_file in sorted([ (int(a.split("-")[-1].split(".")[0]),  a) for a in os.listdir(out_dir+"/") if a.startswith(out_file_base)], key=itemgetter(0)):
-            # Create name to which the current pdftocairo output will be renamed.
-            new_png_file = out_dir+"/"+png_file[:png_file.rfind("-")]+"-"+str(i).zfill(3)+".png"
-            if os.path.isfile(new_png_file):
-                print("Removing existing target "+new_png_file, file=sys.stderr)
-                os.remove(new_png_file)
-                continue
-            if new_png_file not in png_paths:
-                png_paths.append(new_png_file)
-                if verbose: print("\t\t\t"+png_paths[-1], file=sys.stderr)
-                os.replace(out_dir+"/"+png_file, png_paths[-1])
-    else:
+    # if newly_created:
+    #     print("\t\tRenaming images ...", file=sys.stderr)
+    #     # Get all images relating to the current file. 
+    #     # This will also include any *previous* results that were renamed already!
+    #     for i, png_file in sorted([ (int(a.split("-")[-1].split(".")[0]),  a) for a in os.listdir(out_dir+"/") if a.startswith(out_file_base)], key=itemgetter(0)):
+    #         # Create name to which the current pdftocairo output will be renamed.
+    #         new_png_file = out_dir+"/"+png_file[:png_file.rfind("-")]+"-"+str(i).zfill(3)+".png"
+    #         if os.path.isfile(new_png_file):
+    #             print("Removing existing target "+new_png_file, file=sys.stderr)
+    #             os.remove(new_png_file)
+    #             continue
+    #         if new_png_file not in png_paths:
+    #             png_paths.append(new_png_file)
+    #             if verbose: print("\t\t\t"+png_paths[-1], file=sys.stderr)
+    #             os.replace(out_dir+"/"+png_file, png_paths[-1])
+    if not newly_created:
         print("Using existing images ...", file=sys.stderr)
         for i, png_file in sorted([ (int(a.split("-")[-1].split(".")[0]),  a) for a in os.listdir(out_dir+"/") if a.startswith(out_file_base)], key=itemgetter(0)):
             png_paths.append(out_dir+"/"+png_file)
